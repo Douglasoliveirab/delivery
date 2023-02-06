525b796e5b5871ebb4bd119f4babd6a231c22e88
d.beserra <d.beserra@picalike.com>
2023-02-06 08:14:25 -0300

    criado o arquivo gitignore e string mongodb

--- .env/conexao.php
+++ .env/conexao.php
@@ -4,3 +4,8 @@ $conexao = new PDO ('mysql:host=localhost;dbname=delivery',"root","",);
 // if(isset($conexao)){
 //  echo "true";
 // }
+//conexao com mongo db
+// $conexao = ('mongodb+srv://dgDev:dg199506@cluster0.y52ltm8.mongodb.net/?retryWrites=true&w=majority');
+// if(isset($conexao)){
+//      echo "true";
+//     }
