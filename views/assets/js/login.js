d5d87aa4a2b9b72d8bb1f312680fff58561b02d8
d.beserra <d.beserra@picalike.com>
2023-02-14 06:49:40 -0300

    implementado js no login

--- views/assets/js/login.js
+++ views/assets/js/login.js
@@ -3,13 +3,13 @@ $("#newUser").click(function(e){
     e.preventDefault();
     $(".newConta").show();
     $("#logar").hide();
-    console.log("teste");
+  
   });
 
   $("#nextLogar").click(function(e){
     e.preventDefault();
     $(".newConta").hide();
     $("#logar").show();
-    console.log("teste");
+    
   });
 });
