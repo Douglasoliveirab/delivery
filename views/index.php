03f8e6708f3d8fa00ae6cda91b116eca000e6de3
d.beserra <d.beserra@picalike.com>
2023-03-20 18:30:14 -0300

    fix:refatorei o codigo e organizei o projeto

--- views/index.php
+++ views/index.php
@@ -15,7 +15,7 @@
         <head>
             <meta charset="UTF-8">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <link rel="stylesheet" href="../assets/css/index.css">
+            <link rel="stylesheet" href="assets/css/index.css">
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
             <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
