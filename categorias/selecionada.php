49dc2f1bbc05ae99062b0914c11c04a70b76c504
d.beserra <d.beserra@picalike.com>
2023-03-20 16:27:22 -0300

    feat:vriado interface para o carrinho

--- categorias/selecionada.php
+++ categorias/selecionada.php
@@ -15,7 +15,7 @@ $query = $select->fetchAll();
 foreach ($query as $produtos) {
     echo 'nome:  '. $produtos["nome_produto"].' <br/>
     descrição:'. $produtos["descricao"].'  <br/>
-    R$  '. number_format($produtos['valor'],2,",",".").'<br/>
+    R$  '. number_format($produtos['preco'],2,",",".").'<br/>
     <a href="../views/add_carrinho.php?add=carrinho&id='.$produtos["id_produto"].'">Adicionar</a>
     <hr/>';
 }
