3312e6fe186050f15481704973185b84213674a1
d.beserra <d.beserra@picalike.com>
2023-03-28 10:52:38 -0300

    fix:atualizada a pagina de status do pagamento

--- .env/conexao.php
+++ .env/conexao.php
@@ -1,6 +1,6 @@
 <?php
 //conexao com banco mysql
-$conexao = new PDO ('mysql:host=localhost;dbname=lanchonete',"root","",);
+$conexao = new PDO ('mysql:host=localhost;dbname=lanchonete',"root","");
 //  if(isset($conexao)){
 //  echo "conectado";
 // }
--- views/notification.php
+++ views/notification.php
@@ -2,20 +2,40 @@
 include "../.env/conexao.php";
 require '../vendor/autoload.php';
 
-// Configura as credenciais de acesso à API
-MercadoPago\SDK::setAccessToken("TEST-1472282048459445-032409-8723148853628c0116c89b140f329544-1337839420");
-if(isset($_GET) && $_GET['collection_id'] !== "" || null){
-   // Obtém o ID do pagamento a partir do parâmetro GET
-$collection_id = $_GET['collection_id'];
-
-// Busca o pagamento na API do Mercado Pago
-$payment = MercadoPago\Payment::find_by_id($collection_id);
-
-// Obtém o status do pagamento
-$status_pagamento = $payment->status;
-
-// Obtém o número do pedido a partir do parâmetro GET
+//GWT para identificar o numero do peido
 $numero_pedido = $_GET['external_reference'];
+ // Credenciais de acesso à API do Mercado Pago
+$access_token = 'TEST-1472282048459445-032409-8723148853628c0116c89b140f329544-1337839420';
+// $client_id = 'SEU_CLIENT_ID';
+
+// ID da transação que você deseja consultar o status
+$id_transacao = $_GET['collection_id'];
+
+
+// URL da API com os parâmetros necessários
+$url = 'https://api.mercadopago.com/v1/payments/' . $id_transacao . '?access_token=' . $access_token;
+
+// Faz a requisição GET utilizando a função cURL do PHP
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => $url,
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_HTTPHEADER => array(
+    'Accept: application/json'
+  )
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+
+// Converte a resposta JSON em um objeto do PHP
+$dados_transacao = json_decode($response);
+
+// Obtém o status da transação
+$status_pagamento = $dados_transacao->status;
+
 
 //Atualiza o status do pedido na tabela pedidos
 $sql = "UPDATE pedidos SET status_pagamento = :status_pagamento WHERE numero_pedido = :numero_pedido";
@@ -31,10 +51,8 @@ header('Refresh: 5; URL=index.php');
 
 // Exibe uma mensagem para o usuário informando sobre o redirecionamento
 echo "Você será redirecionado para a página destino em 5 segundos...";
-}else{
-  header('Refresh: 5; URL=index.php');
-  echo "o seu pagamento falhou";
-}
+
+
 
 
 
