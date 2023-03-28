9cb0b4c491f7716c11abc2610c923f0720075b47
d.beserra <d.beserra@picalike.com>
2023-03-28 16:06:33 -0300

    feat:criado o script para criar o produto

--- painel/controllers/recebe_produto.php
+++ painel/controllers/recebe_produto.php
@@ -1,28 +1,53 @@
 <?php
+session_start();
+include "../../.env/conexao.php";
+echo "nome do produto: ".$_POST['nome_produto']."<br>";
+echo "imagem do produto: ".$_FILES['img_produto']['name']."<br>";
+echo "descrição: ".$_POST['descricao']."<br>";
+echo "custo: ".$_POST['custo_produto']."<br>";
+echo "preço: ".$_POST['preco']."<br>";
+echo "categoria: ".$_POST['id_categoria']."<br>";
 
-require_once('../../.env/conexao.php');
 
-        // // Verifica se é uma imagem válida
-        // $tipo = $_FILES['foto']['type'];
-        // if($tipo == 'image/jpeg' || $tipo == 'image/png') {
-        //     // Move o arquivo para o diretório de uploads
-        //     $nome_arquivo = $_FILES['foto']['name'];
-        //     $caminho_arquivo = 'uploads/' . $nome_arquivo;
-        //     move_uploaded_file($_FILES['foto']['tmp_name'], $caminho_arquivo);
-        //     // Insere as informações da foto na tabela fotos
-        //     $sql = "INSERT INTO produtos (img_produto, nome_produto,descricao,valor,custo_produto,categoria) VALUES (?,?,?,?,?,?)";
-        //     $stmt = $conexao->prepare($sql);
-        //     $stmt->bindParam(1, $img_produto);
-        //     $stmt->bindParam(2, $nome_produto);
-        //     $stmt->bindParam(3, $descricao);
-        //     $stmt->bindParam(4, $valor);
-        //     $stmt->bindParam(5, $custo_produto);
-        //     $stmt->bindParam(6, $categoria);
-        //     $stmt->execute();
-            
-        // } else {
-        //     echo 'Apenas imagens JPEG e PNG são permitidas.';
-        // }
-        // echo "<script>window.location.href='novoproduto.php'</script>";
-    
+// Verifica se é uma imagem válida
+$tipo = $_FILES['img_produto']['type'];
+if($tipo == 'image/jpeg' || $tipo == 'image/png') {
+
+  // Verifica se a pasta "produtos" existe, se não existir, cria a pasta
+  if (!file_exists("produtos")) {
+      mkdir("produtos");
+  }
+
+  // Move o arquivo para o diretório de uploads
+  $caminho_img_produto = $_FILES['img_produto']['name'];
+  $img_produto = 'produtos/' . $caminho_img_produto;
+  move_uploaded_file($_FILES['img_produto']['tmp_name'], $img_produto);
+  
+  // Insere as informações do produto na tabela produtos
+  $nome_produto = $_POST['nome_produto'];
+  $descricao = $_POST['descricao'];
+  $custo_produto = $_POST['custo_produto'];
+  $preco = $_POST['preco'];
+  $id_categoria = $_POST['id_categoria'];
+
+  $sql = "INSERT INTO produtos (nome_produto, img_produto, descricao, custo_produto, preco, id_categoria) VALUES (?, ?, ?, ?, ?, ?)";
+  $stmt = $conexao->prepare($sql);
+  $stmt->bindParam(1, $nome_produto);
+  $stmt->bindParam(2, $img_produto);
+  $stmt->bindParam(3, $descricao);
+  $stmt->bindParam(4, $custo_produto);
+  $stmt->bindParam(5, $preco);
+  $stmt->bindParam(6, $id_categoria);
+  $stmt->execute();
+
+  
+} else {
+  echo 'Apenas imagens JPEG e PNG são permitidas.';
+}
+// Espera 5 segundos antes de redirecionar para a página destino
+header('Refresh: 4; URL=../produtos.php');
+
+// Exibe uma mensagem para o usuário informando sobre o redirecionamento
+echo "produto cadstrado você sera redirecionado em 4 segundos...";
+
 ?>
---
+++ painel/produtos.php
@@ -0,0 +1,54 @@
+<?php
+session_start();
+// include('login/verifica_sessao.php');
+
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Painel adiministrador</title>
+</head>
+<body>
+
+
+<form method="POST" enctype="multipart/form-data" action="controllers/recebe_produto.php">
+  <label for="nome_produto">Nome do Produto:</label>
+  <input type="text" id="nome_produto" name="nome_produto" required>
+
+  <label for="img_produto">Imagem do Produto:</label>
+  <input type="file" id="img_produto" name="img_produto" required accept="image/jpeg,image/png">
+
+  <label for="descricao">Descrição do Produto:</label>
+  <textarea id="descricao" name="descricao" required></textarea>
+
+  <label for="custo_produto">Custo do Produto:</label>
+  <input type="number" id="custo_produto" name="custo_produto"  required>
+
+  <label for="preco">Preço do Produto:</label>
+  <input type="number" id="preco" name="preco" step="0.01" required>
+
+  <label for="categoria_id">Categoria:</label>
+<select name="id_categoria" id="categoria_id">
+    <?php
+         include "../.env/conexao.php";
+		$select = $conexao->prepare("SELECT * FROM categoria");
+		$select->execute();
+		$categorias = $select->fetchAll();
+        
+
+        foreach($categorias as $categoria) {
+            echo "<option value='".$categoria['id_categoria']."'>".$categoria['nome_categoria']."</option>";
+        }
+
+		
+    ?>
+</select>
+  <input type="submit" value="Adicionar Produto">
+</form>
+
+
+
+</html>
