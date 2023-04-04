a97fc640aa5d5b20961f9a73e045416aee724a2d
d.beserra <d.beserra@picalike.com>
2023-04-04 08:32:02 -0300

    fix: adicionado campos com data de criacao e updat

--- lanchonet.sql
+++ lanchonet.sql
@@ -1,6 +1,6 @@
 -- CREATE DATABASE lanchonete;
 
-USE lanchonete;
+-- USE lanchonete;
 
 CREATE TABLE clientes (
   id_cliente INT(11) NOT NULL AUTO_INCREMENT,
@@ -11,6 +11,8 @@ CREATE TABLE clientes (
   telefone VARCHAR(20) NOT NULL,
   endereco VARCHAR(100) NOT NULL,
   senha VARCHAR(100) NOT NULL,
+  criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  atualizado_em TIMESTAMP DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (id_cliente)
 );
 
@@ -21,6 +23,9 @@ CREATE TABLE enderecos (
   BAIRRO VARCHAR (20) NOT NULL,
   ENDERECO VARCHAR (35) NOT NULL,
   COMPLEMENTO VARCHAR (20),
+  criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  atualizado_em TIMESTAMP DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (id_endereco),
   id_cliente INT(11) NOT NULL,
   FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente)
 );
@@ -29,6 +34,8 @@ CREATE TABLE categoria (
   id_categoria INT(11) NOT NULL AUTO_INCREMENT,
   img_categoria VARCHAR(100) NOT NULL,
   nome_categoria VARCHAR(50) NOT NULL,
+  criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  atualizado_em TIMESTAMP DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (id_categoria)
 );
 
@@ -41,6 +48,8 @@ CREATE TABLE produtos (
   custo_produto DECIMAL(10,2),
   preco DECIMAL(10,2) NOT NULL,
   status_produto VARCHAR(10) NOT NULL,
+  criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  atualizado_em TIMESTAMP DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (id_produto),
   FOREIGN KEY (id_categoria) REFERENCES categoria(id_categoria)
 );
@@ -56,6 +65,8 @@ CREATE TABLE pedidos (
   valor_total DECIMAL(10,2) NOT NULL,
   status_pagamento VARCHAR(20) NOT NULL,
   status_pedido VARCHAR(20) NOT NULL,
+  criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  atualizado_em TIMESTAMP DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (id_pedido),
   FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente)
 );
@@ -65,6 +76,8 @@ CREATE TABLE itens_pedido (
   id_pedido INT(11) NOT NULL,
   id_produto INT(11) NOT NULL,
   quantidade INT(11) NOT NULL,
+  criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  atualizado_em TIMESTAMP DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (id_itens_pedido),
   FOREIGN KEY (id_pedido) REFERENCES pedidos(id_pedido),
   FOREIGN KEY (id_produto) REFERENCES produtos(id_produto)
@@ -75,6 +88,8 @@ CREATE TABLE administrador (
   nome_usuario VARCHAR(50) NOT NULL,
   senha_usuario VARCHAR(50) NOT NULL,
   privilegios VARCHAR(50) NOT NULL,
+  criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  atualizado_em TIMESTAMP DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (id_administrador)
 );
 
@@ -82,5 +97,7 @@ CREATE TABLE banner (
   id_banner INT(11) NOT NULL AUTO_INCREMENT,
   img_banner VARCHAR(50) NOT NULL,
   caminho_banner VARCHAR(100) NOT NULL,
+  criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  atualizado_em TIMESTAMP DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (id_banner)
 );
