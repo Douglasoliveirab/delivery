b82f8583bcb191ae6c061b90e8e70a462bb74ced
d.beserra <d.beserra@picalike.com>
2023-05-17 14:27:23 -0300

    fix: corrigido modal endereco

--- views/itens.php
+++ views/itens.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+include "cabecalho.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,9 +12,21 @@ session_start();
     <title>produtos</title>
 </head>
 <body>
+   <!-- Modal HTML -->
+   <form method="post" action="atualiza_endereco.php">
+        <div class="custom-modal">
+            <div class="custom-modal-content">
+            <span class="modal-close-mob">&times;</span>
+                <h3 class="custom-modal-title">Editar Endereço</h3>
+                <input type="hidden" name="id_cliente" value="<?= $id_cliente ?>" />
+                <input type="text" class="custom-modal-input" id="novo_endereco" name="novo_endereco" placeholder="Digite o novo endereço" value="<?=$endereco?>">
+                <button type="submit" class="custom-modal-btn"> Atualizar </button>
+            </div>
+        </div>
+    </form>
 <?php
 
-include "cabecalho.php";
+
 if (isset($_GET['id_categoria']) && is_numeric($_GET['id_categoria'])) {
     
     $idCategoria = $_GET['id_categoria'];
@@ -26,11 +39,30 @@ $select->execute();
 $query = $select->fetchAll();
 
 
-if (isset($_SESSION['endereco']) && $_SESSION['endereco'] != "") {
-    echo ' <div class="localizacao">
-                     <i class="bi bi-geo-alt" id="btn-busca"></i>' . $endereco . '
-                  </div>';
+ // Verificar se o usuário está logado e possui um ID de cliente
+ if (isset($_SESSION['id_cliente'])) {
+  $id_cliente = $_SESSION['id_cliente'];
+
+  // Consultar o endereço do cliente na tabela clientes
+  $stmt = $conexao->prepare("SELECT endereco FROM clientes WHERE id_cliente = :id_cliente");
+  $stmt->bindParam(':id_cliente', $id_cliente);
+  $stmt->execute();
+
+  // Verificar se a consulta retornou algum resultado
+  if ($stmt->rowCount() > 0) {
+      $row = $stmt->fetch(PDO::FETCH_ASSOC);
+      $_SESSION['endereco'] = $row['endereco'];
+      $endereco = $_SESSION['endereco'];
+
+      // Usar o valor do endereço como desejar
+      echo '<div class="localizacao">
+      <i class="bi bi-geo-alt">Endereço de entrega</i><br>
+      <span class="endereco">' . $endereco . '</span>
+      <i class="bi bi-pencil editar" style="color:red" id="btn-editar">editar</i>
+  </div>';
+  } 
 }
+
 echo '<div class="container_produtos">';
 foreach ($query as $produtos) {
     echo '<div class="produto">
@@ -50,5 +82,37 @@ foreach ($query as $produtos) {
 }
 echo '</div>';
 ?>
+
+  <script>
+      // Exibir o modal e preencher o campo com o valor atual do endereço
+      $('#btn-editar').click(function() {
+            
+            $('.custom-modal').show();
+        });
+
+
+        $('.modal-close-mob').click(function() {
+                $('.custom-modal').css('display', 'none');
+            });
+
+         // Abrir o modal ao clicar no botão "Editar"
+         $('#edit-address').click(function(e) {
+                e.preventDefault();
+                $('#address-modal').css('display', 'block');
+            });
+
+            // Fechar o modal ao clicar no botão de fechar ou fora do modal
+            $('.modal-close').click(function() {
+                $('#address-modal').css('display', 'none');
+            });
+            $(window).click(function(event) {
+                if (event.target == document.getElementById('address-modal')) {
+                    $('#address-modal').css('display', 'none');
+                }
+            });
+
+    
+</script>
+  </script> 
 </body>
 </html>
