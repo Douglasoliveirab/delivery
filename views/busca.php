aa35fa84478c57269f944220674f6f9b54094f31
d.beserra <d.beserra@picalike.com>
2023-06-21 09:40:41 -0300

    fix: corrigido layout para caso nao ache prdouto

--- views/busca.php
+++ views/busca.php
@@ -17,8 +17,46 @@ if (isset($_POST['search-produto']) && !empty($_POST['search-produto'])) {
         exit();
     } else {
         // Exibir mensagem de nenhum produto encontrado
-        echo 'Nenhum produto encontrado.';
-        echo '<a href="../views/">Voltar à página inicial</a>';
+        echo '<!DOCTYPE html>
+        <html>
+        <head>
+            <meta charset="UTF-8">
+            <title>Nenhum produto encontrado</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+            <style>
+                body {
+                    background-color: #f1f1f1;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    height: 100vh;
+                }
+                
+                .container {
+                    width: 400px;
+                    padding: 20px;
+                    background-color: #fff;
+                    text-align: center;
+                }
+                
+                .container i {
+                    font-size: 100px;
+                    color: gray;
+                }
+            </style>
+        </head>
+        <body>
+            <div class="container">
+                <i class="fas fa-shopping-basket"></i>
+                <p>Nenhum produto encontrado.</p>
+                <a href="../views/">Voltar à página inicial</a>
+            </div>
+        
+            <script src="https://kit.fontawesome.com/SEU_CODIGO.js" crossorigin="anonymous"></script>
+        </body>
+        </html>
+        ';
     }
 }else{
     header('location: index.php');
